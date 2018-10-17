df135b8939601a2dc6ce12ba102e1e771916ca6c
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-10-17 20:15:02 -0300

    Fix lftp from uploading empty dirs

--- conda-recipes/pygdf-feedstock/ci_support/ftp_upload.sh
+++ conda-recipes/pygdf-feedstock/ci_support/ftp_upload.sh
@@ -7,10 +7,11 @@ FTP_HOST='oplab9.parqtec.unicamp.br'
 LOCALPATH=$FEEDSTOCK_ROOT/build_artefacts
 REMOTEPATH='/ppc64el/power-ai'
 
+# Upload .tar.bz2 files from LOCALPATH recursively to REMOTEPATH
 lftp -f "
 set dns:order "inet"
 open ftp://$FTP_HOST
 user $FTP_USER $FTP_PASSWORD
-mirror -R --continue --reverse -i \.tar.bz2$ $LOCALPATH $REMOTEPATH
+mirror -R --continue --reverse --no-empty-dirs -i \.tar.bz2$ $LOCALPATH $REMOTEPATH
 bye
 "
