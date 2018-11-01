47fddb6d49cca12ab9b42f3c1541f2e7da0ac0dd
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-11-01 09:22:23 -0300

    Unifying Travis recipes

--- conda-recipes/pygdf-feedstock/ci_support/ftp_upload.sh
+++ conda-recipes/pygdf-feedstock/ci_support/ftp_upload.sh
@@ -10,6 +10,8 @@ REMOTEPATH='/ppc64el/power-ai'
 # Upload .tar.bz2 files from LOCALPATH recursively to REMOTEPATH
 lftp -f "
 set dns:order "inet"
+set xfer:use-temp-file yes
+set xfer:temp-file-name *.tmp
 open ftp://$FTP_HOST
 user $FTP_USER $FTP_PASSWORD
 mirror -R --continue --reverse --no-empty-dirs --no-perms -i \.tar.bz2$ $LOCALPATH $REMOTEPATH
