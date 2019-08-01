51097bc39510a270b4014e01ee0502e85fe77c34
Jason Furmanek <furmanek@us.ibm.com>
2019-08-01 05:29:17 +0000

    arch specific, no upload for now

--- conda-recipes/tensorflow-hub-feedstock/ci_support/build_steps.sh
+++ conda-recipes/tensorflow-hub-feedstock/ci_support/build_steps.sh
@@ -33,8 +33,8 @@ make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 conda build "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
     --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml"
 
-if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
-    upload_package "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
-fi
+#if [[ "${UPLOAD_PACKAGES}" != "False" ]]; then
+#    upload_package "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
+#fi
 
 touch "/home/conda/feedstock_root/build_artifacts/conda-forge-build-done-${CONFIG}"
