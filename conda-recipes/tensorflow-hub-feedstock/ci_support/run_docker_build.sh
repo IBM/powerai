d7d27fccc2f34dc206063de8d6c49b5ddb3a8745
Jason Furmanek <furmanek@us.ibm.com>
2019-08-01 21:26:30 +0000

    License update and cleanup

--- conda-recipes/tensorflow-hub-feedstock/ci_support/run_docker_build.sh
+++ conda-recipes/tensorflow-hub-feedstock/ci_support/run_docker_build.sh
@@ -1,3 +1,14 @@
+# *****************************************************************
+#
+# Licensed Materials - Property of IBM
+#
+# (C) Copyright IBM Corp. 2018, 2019. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# *****************************************************************
+
 #!/usr/bin/env bash
 
 THISDIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
