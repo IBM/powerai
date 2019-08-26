819ed6595af4590f991338b600a84566bd8fdc65
jayfurmanek <furmanek@us.ibm.com>
2019-08-26 00:53:41 -0500

    Update libsm (#70)

--- conda-recipes/libsm-cos7-ppc64le-feedstock/recipe/build.sh
+++ conda-recipes/libsm-cos7-ppc64le-feedstock/recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/ppc64le-conda_cos7-linux-gnu/sysroot/usr
-pushd ${PREFIX}/ppc64le-conda_cos7-linux-gnu/sysroot/usr > /dev/null 2>&1
+mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/usr
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/usr > /dev/null 2>&1
 cp -Rf "${SRC_DIR}"/binary/* .
