7063cc2a19e89ca06be40a2c42c5b2ecf8825199
Jason Furmanek <furmanek@us.ibm.com>
2019-08-25 23:11:54 -0500

    fix libxt-devel recipe

--- conda-recipes/libxt-devel-cos7-ppc64le-feedstock/recipe/build.sh
+++ conda-recipes/libxt-devel-cos7-ppc64le-feedstock/recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/ppc64le-conda_cos7-linux-gnu/sysroot/usr
-pushd ${PREFIX}/ppc64le-conda_cos7-linux-gnu/sysroot/usr > /dev/null 2>&1
-cp -Rf "${SRC_DIR}"/binary/* .
+RPM=$(find ${PWD}/binary -name "*.rpm")
+mkdir -p ${PREFIX}/powerpc64le-conda_cos6-linux-gnu/sysroot
+pushd ${PREFIX}/powerpc64le-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+  "${RECIPE_DIR}"/rpm2cpio "${RPM}" | cpio -idmv
+popd > /dev/null 2>&1
