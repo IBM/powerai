da8d5a4cab27a6316f9bfdc147f4c40dea124f0f
Nishidha <nishidha@us.ibm.com>
2019-09-11 21:38:13 +0530

    Sentencepiece recipe (#76)
    * Added recipe for sentencepiece
    
    * CI change for sentencepiece
    
    * Added make as a dependency
    
    * Corrected order of export variables
    
    * Fixed the build
    
    * Use PYTHON env variable in recipe
    
    * Fixed the build
    
    * fixed the build and runtime issue
    
    * Fix build
    
    * An attempt to fix the build
    
    * One more attempt to fix the issues
    
    * Tried using patchelf
    
    * Some more trials
    
    * Minor correction
    
    * Fixed path of binary installed
    
    * Updated version to not have v in it

---
+++ conda-recipes/sentencepiece-feedstock/recipe/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# (C) Copyright IBM Corp. 2019. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *****************************************************************
+
+set -ex
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} .. -DSPM_BUILD_TEST=ON -DSPM_ENABLE_TENSORFLOW_SHARED=ON
+make -j $(nproc)
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
+make install
+cd ../python
+
+python setup.py install
+
+SYS_PYTHON_MAJOR=$(python -c "import sys;print(sys.version_info.major)")
+SYS_PYTHON_MINOR=$(python -c "import sys;print(sys.version_info.minor)")
+patchelf --set-rpath $LD_LIBRARY_PATH $PREFIX/lib/python${SYS_PYTHON_MAJOR}.${SYS_PYTHON_MINOR}/site-packages/sentencepiece-$PKG_VERSION-py${SYS_PYTHON_MAJOR}.${SYS_PYTHON_MINOR}-linux-ppc64le.egg/_sentencepiece.cpython-${CONDA_PY}m-powerpc64le-linux-gnu.so
+
+cd ../tensorflow
+python setup.py install
