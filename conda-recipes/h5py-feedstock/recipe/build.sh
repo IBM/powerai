74092111b1534a551d54ba33d737f6af18bfa856
William D. Irons <wdirons@us.ibm.com>
2019-12-04 09:38:57 -0600

    h5py-feedstock conda recipe
    Based on https://github.com/AnacondaRecipes/h5py-feedstock
    Only for ppc64le, as 2.9.0 is available from Anaconda for x86_64

--- conda-recipes/h5py-feedstock/recipe/build.sh
+++ conda-recipes/h5py-feedstock/recipe/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-mpi_arg=""
-if [[ "$mpi" != "nompi" ]]; then
-  mpi_arg="--mpi"
-fi
-
-"${PYTHON}" setup.py configure $mpi_arg --hdf5="${PREFIX}"
-"${PYTHON}" -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+"${PYTHON}" setup.py configure --hdf5="${PREFIX}"
+"${PYTHON}" -m pip install . --no-deps --ignore-installed -vvv
--- conda-recipes/h5py-feedstock/recipe/run_test.py
+++ conda-recipes/h5py-feedstock/recipe/run_test.py
@@ -1,9 +1,3 @@
-import os
-
-os.environ['OMPI_MCA_plm'] = 'isolated'
-os.environ['OMPI_MCA_btl_vader_single_copy_mechanism'] = 'none'
-os.environ['OMPI_MCA_rmaps_base_oversubscribe'] = 'yes'
-
 import h5py
 import h5py._conv
 import h5py._errors
@@ -26,10 +20,20 @@ import h5py.h5t
 import h5py.h5z
 import h5py.utils
 
-# verify that mpi builds are built with mpi
-should_have_mpi = os.getenv('mpi', 'nompi') != 'nompi'
-have_mpi = h5py.get_config().mpi
-assert have_mpi == should_have_mpi, "Expected mpi=%r, got %r" % (should_have_mpi, have_mpi)
+import numpy as np
 
 from sys import exit
-exit(h5py.run_tests())
+
+# test file read/write, regression test for https://github.com/ContinuumIO/anaconda-issues/issues/7686
+fh = h5py.File('test.h5', 'w')
+fh.create_group('/top')
+fh['/top'].attrs['attr1'] = 'Test Attribute 1'
+fh['/top'].attrs['attr2'] = 'Test Attribute 2'
+fh['/top'].create_dataset('image', data=np.arange(1000)/3.0)
+fh.flush()
+fh.close()
+
+fh = h5py.File('test.h5', 'r')
+fh['/top'].attrs['attr1']
+
+exit(0) if h5py.run_tests().wasSuccessful() else exit(1)
