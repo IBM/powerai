54313a5a35c54eefd93298cd366fdec21b556e24
Nishidha <nishidha@us.ibm.com>
2020-01-29 20:43:14 +0530

    Update build_steps.sh
    Reverted characters which got inserted mistakenly.

--- conda-recipes/tensor2tensor-feedstock/ci_support/build_steps.sh
+++ conda-recipes/tensor2tensor-feedstock/ci_support/build_steps.sh
@@ -1,4 +1,6 @@
-# (C) Copyright IBM Corp. 2018, 202020202020202020202020202020202020202020der the Apache License, Version 2.0 (the "License");
+# (C) Copyright IBM Corp. 2018, 2020. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
