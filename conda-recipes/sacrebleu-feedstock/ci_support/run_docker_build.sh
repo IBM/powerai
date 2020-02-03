a800239fdb7a58ef7272f8bf40f39a1a7fa0f731
Deepali Chourasia <deepali@us.ibm.com>
2020-02-03 13:39:00 +0000

    use tty

--- conda-recipes/sacrebleu-feedstock/ci_support/run_docker_build.sh
+++ conda-recipes/sacrebleu-feedstock/ci_support/run_docker_build.sh
@@ -37,6 +37,7 @@ mkdir -p "$ARTIFACTS"
 DONE_CANARY="$ARTIFACTS/conda-forge-build-done-${CONFIG}"
 rm -f "$DONE_CANARY"
 # Enable running in interactive mode attached to a tty
+test -t 1 && USE_TTY="-t"
 DOCKER_RUN_ARGS=" -i ${USE_TTY} "
 
 if [ -z "${DOCKER_IMAGE}" ]; then
