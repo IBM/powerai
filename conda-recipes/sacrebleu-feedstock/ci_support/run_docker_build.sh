f5a30e0b8de55ddd384eaabd87a956c0c8876e74
Deepali Chourasia <deepali@us.ibm.com>
2020-02-03 12:24:14 +0000

    use tty

--- conda-recipes/sacrebleu-feedstock/ci_support/run_docker_build.sh
+++ conda-recipes/sacrebleu-feedstock/ci_support/run_docker_build.sh
@@ -37,7 +37,7 @@ mkdir -p "$ARTIFACTS"
 DONE_CANARY="$ARTIFACTS/conda-forge-build-done-${CONFIG}"
 rm -f "$DONE_CANARY"
 # Enable running in interactive mode attached to a tty
-DOCKER_RUN_ARGS=" -it "
+DOCKER_RUN_ARGS=" -i ${USE_TTY} "
 
 if [ -z "${DOCKER_IMAGE}" ]; then
   echo "WARNING: DOCKER_IMAGE variable not set. Falling back to condaforge/linux-anvil-ppc64le"
