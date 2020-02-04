510e898423f062fcb22e8d21719a3de541346d42
Deepali Chourasia <deepali@us.ibm.com>
2020-02-04 12:48:53 +0000

    Revert "remove tty change"
    This reverts commit cf54e35819e10019b2841904f3be6e7aa23b4f28.

--- conda-recipes/pytorch-pretrained-bert-feedstock/ci_support/run_docker_build.sh
+++ conda-recipes/pytorch-pretrained-bert-feedstock/ci_support/run_docker_build.sh
@@ -37,7 +37,8 @@ mkdir -p "$ARTIFACTS"
 DONE_CANARY="$ARTIFACTS/conda-forge-build-done-${CONFIG}"
 rm -f "$DONE_CANARY"
 # Enable running in interactive mode attached to a tty
-DOCKER_RUN_ARGS=" -it "
+test -t 1 && USE_TTY="-t"
+DOCKER_RUN_ARGS=" -i ${USE_TTY} "
 
 if [ -z "${DOCKER_IMAGE}" ]; then
   echo "WARNING: DOCKER_IMAGE variable not set. Falling back to condaforge/linux-anvil-ppc64le"
