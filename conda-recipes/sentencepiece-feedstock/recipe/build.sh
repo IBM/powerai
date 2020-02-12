7ac5bd633e37ea095e673c86fda4b72423424684
Nishidha Panpaliya <nishidha@us.ibm.com>
2020-02-12 14:20:33 +0000

    Updated sentencepiece to generate TF plugin library and refreshed to the latest release

--- conda-recipes/sentencepiece-feedstock/recipe/build.sh
+++ conda-recipes/sentencepiece-feedstock/recipe/build.sh
@@ -17,6 +17,47 @@
 
 set -ex
 
+build_tf_wrapper() {
+  if [ "$1" != "" ]; then
+    pkg_name="==$1"
+  fi
+
+  export IBM_POWERAI_LICENSE_ACCEPT=yes
+  conda create -n tf$1 -y python=3.6
+
+  conda activate tf$1
+  conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+  conda config --prepend channels http://ausgsa.ibm.com/projects/p/powerai-conda/conda-early-access/
+
+  # Builds _sentencepiece_processor_ops.so
+  conda install -y tensorflow${pkg_name}
+
+  TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
+  TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
+  TF_VERSION=( $(python -c 'import tensorflow as tf; print(tf.__version__)') )
+
+  echo TF_CFLAGS=${TF_CFLAGS[@]}
+  echo TF_LFLAGS=${TF_LFLAGS[@]}
+  echo TF_VERSION=${TF_VERSION}
+  CUR_DIR=$(pwd)
+  echo "Current dir: $CUR_DIR"
+
+  conda deactivate
+  
+  g++ -std=c++11 -shared \
+    -I../src \
+    -fPIC ${TF_CFLAGS[@]} -O2 \
+    -Wl,--whole-archive \
+    ${PREFIX}/lib/libsentencepiece.a \
+    -Wl,--no-whole-archive \
+    ../tensorflow/sentencepiece_processor_ops.cc \
+    -o ../tensorflow/tf_sentencepiece/_sentencepiece_processor_ops.so.${TF_VERSION} \
+    ${TF_LFLAGS[@]}
+
+  strip ../tensorflow/tf_sentencepiece/_sentencepiece_processor_ops.so.${TF_VERSION}
+  conda env remove -n tf$1
+}
+
 ARCH=`uname -p`
 if [[ "${ARCH}" == 'ppc64le' ]]; then
     ARCH_SO_NAME="powerpc64le"
@@ -30,9 +71,16 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} .. -DSPM_BUILD_TEST=ON -DSPM_ENABLE_TENSORFLOW_SHARED=ON
 make -j $(nproc)
+
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
 export LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
 make install
+
+build_tf_wrapper "1.13.1"
+build_tf_wrapper "1.14.0"
+build_tf_wrapper "1.15.0"
+build_tf_wrapper "2.1.0"
+
 cd ../python
 
 python setup.py install
