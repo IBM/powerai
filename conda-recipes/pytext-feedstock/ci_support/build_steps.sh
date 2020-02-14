b3410042ee5e922be76bb12d7c6686052178846a
deepali-c <deepali@us.ibm.com>
2020-02-14 20:14:41 +0530

    update to pytext v0.3.1 (#211)

--- conda-recipes/pytext-feedstock/ci_support/build_steps.sh
+++ conda-recipes/pytext-feedstock/ci_support/build_steps.sh
@@ -41,7 +41,7 @@ setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
-conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda-early-access/
 export IBM_POWERAI_LICENSE_ACCEPT=yes
 
 conda build "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
