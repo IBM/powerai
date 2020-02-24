b7c17447c0387236312005e7eeb9dd1787248a8d
William D. Irons <wdirons@us.ibm.com>
2020-02-24 13:43:24 -0600

    Use ibmcom/powerai docker image for build performance
    ibmcom/powerai:1.7.0-pytorch-ubuntu18.04-py37-ppc64le already
    has cudatoolkit installed, so the only large package to install
    is cudatoolkit-dev.

--- conda-recipes/pycuda-feedstock/ci_support/build_steps.sh
+++ conda-recipes/pycuda-feedstock/ci_support/build_steps.sh
@@ -20,6 +20,9 @@ export FEEDSTOCK_ROOT=/home/conda/feedstock_root
 export RECIPE_ROOT=/home/conda/recipe_root
 export CI_SUPPORT=/home/conda/feedstock_root/.ci_support
 export CONFIG_FILE="${CI_SUPPORT}/${CONFIG}.yaml"
+export PATH=/opt/anaconda/bin:$PATH
+
+conda install --yes git
 
 cat >~/.condarc <<CONDARC
 
@@ -28,9 +31,6 @@ conda-build:
 
 CONDARC
 
-conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
-export IBM_POWERAI_LICENSE_ACCEPT=yes
-
 conda install --yes --quiet conda-forge-ci-setup=2 conda-build -c conda-forge
 
 # set up the condarc
@@ -38,6 +38,10 @@ setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
+
+conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+conda config --set channel_priority strict
+export IBM_POWERAI_LICENSE_ACCEPT=yes
 
 conda build "${RECIPE_ROOT}" -m "${CI_SUPPORT}/${CONFIG}.yaml" \
     --clobber-file "${CI_SUPPORT}/clobber_${CONFIG}.yaml" 
